204e416d681589006edd29ddaeedfbe595b37173
Uriel Duran <uduran12@gmail.com>
2017-09-12 16:49:14 -0500

    getbyid select

---
+++ Assignments/Week_3/selectors.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    document.getElementById("1").style.color = "red";
+    document.getElementById("2").style.color = "blue";
+    document.getElementById("3").style.color = "green";
+    document.getElementById("4").style.color = "orange";
+    document.getElementById("5").style.color = "black";
+    document.getElementById("6").style.color = "yellow";
+    document.getElementById("7").style.color = "brown";
+
+
+  });
+
