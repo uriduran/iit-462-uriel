690dde653579e9974e651b376c70f6fc6a394983
Uriel Duran <uduran12@gmail.com>
2017-09-11 22:18:59 -0500

    fizzbuzz assignment

---
+++ Assignments/Week_3/fizzbuzz.js
@@ -0,0 +1,30 @@
+var num1, num2;
+
+num1 = window.prompt("Range: Smallest number", "0");
+num2 = window.prompt("Range: Largest number", "0");
+
+if (num1 <= num2) {//To make sure the user inputs the smaller number first.
+    fizzbuzz(num1,num2);
+    
+    function fizzbuzz(num1, num2) {
+    
+        for (var i = num1; i <= num2; i++) {
+            if (i % 3 == 0 && i % 5 !== 0) {
+                console.log(i + " Fizz");
+            }
+            if (i % 3 !== 0 && i % 5 == 0) {
+                console.log(i + " Buzz");
+            }
+            if (i % 3 == 0 && i % 5 == 0) {
+                console.log(i + " Fizzbuzz");
+            }
+            if (i % 3 !== 0 && i % 5 !== 0) {
+                console.log(i);
+            }
+        }
+        
+    }    
+}else{
+    window.alert("Please input smaller number in the range first.")
+}
+
