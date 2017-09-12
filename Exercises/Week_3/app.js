3f138198a5cdd48b328536c9b1f4226d6cb76129
Uriel Duran <uduran12@gmail.com>
2017-09-11 21:05:12 -0500

    fade in

--- Exercises/Week_3/app.js
+++ Exercises/Week_3/app.js
@@ -4,8 +4,10 @@ var $comment_text = $(".comment-input input");
 
 if ($comment_text.val() !== "") {
     var $new_comment = $("<p>"); //paragraph tags
+
     $new_comment.text($comment_text.val()); //adds the input text to the tag
-    $(".comments").append($new_comment); //adds it all to the .comments selector  
+    $new_comment.fadeIn();
+    $(".comments").append($new_comment); //adds it all to the .comments selector 
     $comment_text.val(""); //value in input is cleared out. 
 }
 });
