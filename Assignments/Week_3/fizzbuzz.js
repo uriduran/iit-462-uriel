ef72631d8daa0327fa793057c83ccae3fc148d2c
Uriel Duran <uduran12@gmail.com>
2017-09-11 22:24:27 -0500

    seperated code to make it more readable

--- Assignments/Week_3/fizzbuzz.js
+++ Assignments/Week_3/fizzbuzz.js
@@ -4,27 +4,25 @@ num1 = window.prompt("Range: Smallest number", "0");
 num2 = window.prompt("Range: Largest number", "0");
 
 if (num1 <= num2) {//To make sure the user inputs the smaller number first.
-    fizzbuzz(num1,num2);
-    
-    function fizzbuzz(num1, num2) {
-    
-        for (var i = num1; i <= num2; i++) {
-            if (i % 3 == 0 && i % 5 !== 0) {
-                console.log(i + " Fizz");
-            }
-            if (i % 3 !== 0 && i % 5 == 0) {
-                console.log(i + " Buzz");
-            }
-            if (i % 3 == 0 && i % 5 == 0) {
-                console.log(i + " Fizzbuzz");
-            }
-            if (i % 3 !== 0 && i % 5 !== 0) {
-                console.log(i);
-            }
-        }
-        
-    }    
+    fizzbuzz(num1,num2); //Calls and executes function if input is correct.
 }else{
     window.alert("Please input smaller number in the range first.")
 }
 
+    
+function fizzbuzz(num1, num2) {
+    for (var i = num1; i <= num2; i++) {
+        if (i % 3 == 0 && i % 5 !== 0) {
+                console.log(i + " Fizz");
+        }
+        if (i % 3 !== 0 && i % 5 == 0) {
+                console.log(i + " Buzz");
+        }
+         if (i % 3 == 0 && i % 5 == 0) {
+                console.log(i + " Fizzbuzz");
+            }
+        if (i % 3 !== 0 && i % 5 !== 0) {
+                console.log(i);
+        }
+    }
+}
