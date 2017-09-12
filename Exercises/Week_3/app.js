d53073be34bdd528d7f79f7d82ad6311720809c5
Uriel Duran <uduran12@gmail.com>
2017-09-11 20:39:25 -0500

    FIRST UPDATE

--- Exercises/Week_3/app.js
+++ Exercises/Week_3/app.js
@@ -1,4 +1,11 @@
-$(".comment-input button").on("click", function () {
-var $newP = $("<p>New Element</p>");
-$(".comments").append($newP);    
+$(".comment-input button").on("click", function () { //on click of button do this
+var $new_comment = $("<p>"); //paragraph tags
+
+var comment_text = $(".comment-input input").val(); //takes in the input text
+
+$new_comment.text(comment_text); //adds the input text to the tag
+
+$(".comments").append($new_comment); //adds it all to the .comments selector
+
 });
+
