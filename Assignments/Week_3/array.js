d91fd92b15ff74c121094528c9d48871d1947315
Uriel Duran <uduran12@gmail.com>
2017-09-12 16:49:25 -0500

    find largest num

---
+++ Assignments/Week_3/array.js
@@ -0,0 +1,16 @@
+var numbers = [1,5,8,9,3];
+var large = 0;
+
+function max(numbers) {
+
+    for (i=0; i <= large; i++) {
+        if (numbers[i]>large) {
+            var large=numbers[i];
+            
+        }
+        
+    }
+
+    console.log(large);
+    
+}
