23acbe1ad18e06e1d9362fd875e829bf6e56ef64
Uriel Duran <uduran12@gmail.com>
2017-09-18 21:00:32 -0500

    create array with hand

---
+++ Assignments/Week_4/cards.js
@@ -0,0 +1,12 @@
+//init empty array
+var cards = [
+    {"rank": "two", "suit":"hearts"},
+    {"rank": "ace", "suit":"spades"},
+    {"rank": "five", "suit":"spades"},
+    {"rank": "king", "suit":"clubs"},
+    {"rank": "seven", "suit":"diamonds"}
+
+];
+
+
+console.log(cards[3]);
