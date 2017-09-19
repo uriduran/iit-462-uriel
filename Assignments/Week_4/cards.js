9c62dcef285df8a030fc92a568732ac2ef8dd9c7
Uriel Duran <uduran12@gmail.com>
2017-09-18 22:18:26 -0500

    two and three of a kind

--- Assignments/Week_4/cards.js
+++ Assignments/Week_4/cards.js
@@ -1,12 +1,54 @@
 //init empty array
-var cards = [
+var hand = [
     {"rank": "two", "suit":"hearts"},
-    {"rank": "ace", "suit":"spades"},
-    {"rank": "five", "suit":"spades"},
+    {"rank": "two", "suit":"spades"},
+    {"rank": "two", "suit":"diamonds"},
     {"rank": "king", "suit":"clubs"},
     {"rank": "seven", "suit":"diamonds"}
 
 ];
 
+//ALL POSSIBLE RANKS
 
-console.log(cards[3]);
+var ranks = ["two", "three", "four", "five", "seven","eight","nine","ten","jack","queen","king","ace"];
+
+var twoPair = false;
+var threeKind = false;
+
+
+function containsNTimes(handR, ranks) {
+        var count = handR.reduce(function(n, val) {
+            return n + (val === ranks);
+        }, 0);
+        
+        return count;
+    };
+
+
+var handRanks;
+//stores all of the ranks in var handRanks
+handRanks = hand.map(function (card) {
+    return card.rank;
+});
+
+
+//TWOPAIR-------------------------------------------------------
+ranks.forEach(function (rank){
+    if (containsNTimes(handRanks, rank) <= 2){
+        twoPair = true;
+    }
+    if(twoPair == true){
+        console.log("Two Pair!");
+    };
+});
+
+//THREE OF A KIND-------------------------------------------------------
+
+ranks.forEach(function (rank){
+    if (containsNTimes(handRanks, rank) == 3){
+        threeKind = true;
+    }
+    if(threeKind == true){
+        console.log("Three of a kind!");
+    };
+});
