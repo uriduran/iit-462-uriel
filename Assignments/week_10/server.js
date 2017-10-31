55a75e2660bb5252e002a21f79bcb652e5d39e55
Uriel Duran <uduran12@gmail.com>
2017-10-31 15:17:47 -0700

    post reminders logic

--- Assignments/week_10/server.js
+++ Assignments/week_10/server.js
@@ -4,6 +4,9 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
+//json
+app.use(bodyParser.json());
+
 //initialize server
 app.listen(3000, function () {
   console.log('Reminder app listening on port 3000')
@@ -15,8 +18,6 @@ app.get('/', function (req, res) {
   res.end("Reminder App!")
 });
 
-//json
-app.use(bodyParser.json());
 
 var users = []; //init users array
 //Create a new user
@@ -28,5 +29,25 @@ app.post('/users', function (req, res){
   users.push(newUsr); //add new user to users array
   res.status(200); //succesful completion
   res.json(id);
+
+})
+
+app.post('/users/:userId/reminders', function (params) {
+  var inputUserId = req.params.userId; //take in user id from post
+  var date = new Date(); //take js date into variable
+
+  var location = inputUserId - 1; //takes user ID and subtracts one for location in array
+
+  if(!users[location]){//if user not found in users array
+    res.status(404);
+    res.json({"message" : "userId not found: " + inputId});
+  }else{
+    var id = {"id" : users[location].reminders.length + 1};
+    var newRem = req.body; //take in body msg
+    newRem.reminder.created = date;
+    users[location].reminders.push(newRem);
+    res.status(200); //succesful
+    res.json(id);
+  }
 
 })
