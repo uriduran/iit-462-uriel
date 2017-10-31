06ae0f9fea661e6e11ef26fd062b2398a3c7ee87
Uriel Duran <uduran12@gmail.com>
2017-10-31 15:06:23 -0700

    create user

--- Assignments/week_10/server.js
+++ Assignments/week_10/server.js
@@ -14,3 +14,19 @@ app.get('/', function (req, res) {
   console.log("Reminder App");
   res.end("Reminder App!")
 });
+
+//json
+app.use(bodyParser.json());
+
+var users = []; //init users array
+//Create a new user
+app.post('/users', function (req, res){
+  var id = {"id" : users.length + 1}; //increments IDs in array by 1.
+  var newUsr = req.body; //take in info from body
+  newUsr.id = id.id;
+  newUsr.reminder = []; //init array to hold reminders
+  users.push(newUsr); //add new user to users array
+  res.status(200); //succesful completion
+  res.json(id);
+
+})
