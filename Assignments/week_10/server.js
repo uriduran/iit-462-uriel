73b189e7b34d347a6fe7ab329e4aeb06f22697cc
Uriel Duran <uduran12@gmail.com>
2017-10-31 14:17:12 -0700

    test server creation

--- Assignments/week_10/server.js
+++ Assignments/week_10/server.js
@@ -4,3 +4,9 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
+const http = require('http');
+http.createServer(function (req, res) {
+  console.log(`${req.method} ${req.url}`);
+  res.end('hello world!');
+}).listen(3000);
+console.log('Server listening on port 3000');
