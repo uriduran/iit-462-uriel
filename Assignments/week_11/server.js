4f8bbc859c026129c502259bb0e68791a28162ba
Uriel Duran <uduran12@gmail.com>
2017-11-06 22:04:51 -0600

    add missing config

--- Assignments/week_11/server.js
+++ Assignments/week_11/server.js
@@ -5,6 +5,13 @@ const app = express()
 //json parse
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With,  X-HTTP-Method-Override, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+  next();
+});
+
 //initialize server
 app.listen(3000, function () {
     console.log('Reminder app listening on port 3000')
